b4b28835e960134b3e809e531a1e6d843dd8f81b
Steffen <user01@RPI-GARAGE.fritz.box>
2020-05-02 23:52:56 +0200

    loop for years until 25 years back

--- copy-pics-mm.sh
+++ copy-pics-mm.sh
@@ -6,38 +6,24 @@ copypics()
 {
 	YMD=$1
 	echo "copypics: $YMD"
-	/bin/cp $SOURCE/$YMD* $TARGET
+	/bin/cp $SOURCE/$YMD* $TARGET > /dev/null 2>&1
 }
 
 #delete pic in target dir
 /bin/rm $TARGET/*.jpg 
 
-#pics 1 year before
-YMD=`/bin/date +%Y-%m-%d`
-YMD=$(/bin/date -d "$YMDi -1 year" +%Y-%m-%d)
-copypics $YMD
+for year in {1..25} ; do
+	YMD=`/bin/date +%Y-%m-%d`
+	YMD=$(/bin/date -d "$YMD -$year years" +%Y-%m-%d)
+	copypics $YMD
+done
 
-#pics 3 years before
-YMD=`/bin/date +%Y-%m-%d`
-YMD=$(/bin/date -d "$YMD -3 years" +%Y-%m-%d)
-copypics $YMD
-
-#pics 5 years before
-YMD=`/bin/date +%Y-%m-%d`
-YMD=$(/bin/date -d "$YMD -5 years" +%Y-%m-%d)
-copypics $YMD
-
-#convert(add partial filename which reflects date stamp) pics in target dir
-
+#convert(add filename) pics in target dir
 for f in $TARGET/* ; do
 	name=`/usr/bin/basename $f`
-	echo "$name - $f"
 	#text postition starting right vertical, right horizontal
 	mogrify -font helvetica -fill red -pointsize 75 -draw "text 20,100 '$name'" $f
 done
 
 #restart of MagicMirror
 /usr/bin/pm2 restart magicmirror-startup-script
-
-
-
