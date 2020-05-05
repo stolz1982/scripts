6471aa489a769c3771511f924368d406838a0bc9
Steffen <user01@RPI-DB.corp.stolz.cloud>
2020-05-05 14:07:39 +0200

    new homes mount script

---
+++ home-mount.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Variablen setzen
+TARGET_DIR="/media/homes/Bilder/00-PICAM-DB"
+CHECK_FILE="check.datei"
+MOUNT_DIR="/media/homes"
+
+if [ -f $TARGET_DIR$CHECK_FILE ]; then
+	
+	/bin/echo "$TARGET_DIR/$CHECK_FILE exists"
+
+else
+	
+	/bin/echo "$TARGET_DIR/$CHECK_FILE does not exist, trying to mount"
+	/bin/mount $MOUNT_DIR
+
+fi
